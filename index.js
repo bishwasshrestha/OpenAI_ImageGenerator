b3fe33282146dfc80496accc9e2c35e1dd2a628f
Bishwas Shrestha <bishwas.shrestha@gmail.com>
2023-02-09 17:17:22 +0200

    image generator basic front end added

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 3000
 const app = express()
-
+const path = require('path')
 //enable body parser
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use('/openai',require('./routes/openaiRoute'))
 
-
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')))
  
 app.listen(PORT,()=> console.log(`server running on port ${PORT}`))
